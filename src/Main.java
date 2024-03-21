965e50b2656a18e032d6e73ef99d4eb307fc77d3
dogusmiuw <dceliknodes@gmail.com>
2024-03-22 01:54:17 +0300

    main organised

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,16 @@
  * @since 1.0
  */
 public class Main {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
         Frame f = new Frame();
 
-        FileTree fileTree = new FileTree(f);
         EditorArea editorArea = new EditorArea();
         Editor editor = new Editor(f, editorArea);
-        StatusBar statusBar = new StatusBar(f);
         ScrollPane scrollPane = new ScrollPane(editor);
+
+        FileTree fileTree = new FileTree(f);
+        StatusBar statusBar = new StatusBar(f);
 
         f.add(scrollPane);
         f.setVisible(true);
